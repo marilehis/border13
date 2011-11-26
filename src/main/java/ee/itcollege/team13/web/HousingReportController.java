b7cf67f51220ca0c4e559cb079042998676b040e
Denis Veltson <dveltson@itcollege.ee>
2011-11-26 12:27:41 +0200

    uuesti

---
+++ src/main/java/ee/itcollege/team13/web/HousingReportController.java
@@ -0,0 +1,31 @@
+package ee.itcollege.team13.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ee.itcollege.team13.domain.House;
+
+@RequestMapping("/housingreport/**")
+@Controller
+public class HousingReportController {
+
+    @RequestMapping
+    public void get(ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "{id}")
+    public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    @RequestMapping
+    public String index(Model uiModel) {
+    	uiModel.addAttribute("houses", House.findAllHouses());
+        return "housingreport/index";
+    }
+}

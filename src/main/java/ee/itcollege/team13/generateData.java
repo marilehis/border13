bd76c33471dc73816ecfdeb1db9c339cf7dee57d
Raul Valge <Raul.Valge@gmail.com>
2011-12-01 14:00:40 +0200

    Generate room types

--- src/main/java/ee/itcollege/team13/generateData.java
+++ src/main/java/ee/itcollege/team13/generateData.java
@@ -3,10 +3,25 @@ package ee.itcollege.team13;
 import java.util.List;
 
 import ee.itcollege.team13.domain.AdminUnit;
+import ee.itcollege.team13.domain.RoomType;
 
 public class generateData {
 
 	public generateData() {
+		genAdminUnits();
+		genRoomTypes();
+	}
+
+	private void genRoomTypes() {
+		List<RoomType> types = RoomType.findAllRoomTypes();
+		if (types == null || types.size() == 0) {
+			RoomType a = new RoomType();
+			a.setRoomTypeId("garnison");
+			a.setName("Garnison");
+		}
+	}
+
+	private void genAdminUnits() {
 		List<AdminUnit> units = AdminUnit.findAllAdminUnits();
 		
 		if (units == null || units.size() == 0) {
